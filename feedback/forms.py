d3f9fa7d1b313550b513c57eab39760fc3668ccf
Oleg Bondarenko <millioner.bbb@gmail.com>
2011-12-23 18:28:53 +0200

    Small changes

--- feedback/forms.py
+++ feedback/forms.py
@@ -3,12 +3,14 @@ from django import forms
 from feedback.models import Feedback, AnonymousFeedback
 
 class FeedbackForm(forms.ModelForm):
+    next = forms.CharField(required=False, widget=forms.HiddenInput)
     class Meta:
         model = Feedback
         exclude = ('user',)
 
 
 class AnonymousFeedbackForm(forms.ModelForm):
+    next = forms.CharField(required=False, widget=forms.HiddenInput)
     class Meta:
         model = AnonymousFeedback
         exclude = ('user',)
--- feedback/views.py
+++ feedback/views.py
@@ -1,18 +1,21 @@
-from django.utils.translation import ugettext as _
-
+from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.utils.translation import ugettext as _
+from django.contrib import messages
 
 from feedback.forms import FeedbackForm
 
 def leave_feedback(request, template_name='feedback/feedback_form.html'):
-    form = FeedbackForm(request.POST or None)
+    form = FeedbackForm(request.POST or None, initial={
+        'next': getattr(settings, 'FEEDBACK_REDIRECT_URL', request.get_full_path())
+    })
     if form.is_valid():
         feedback = form.save(commit=False)
         feedback.user = request.user
         feedback.save()
-        request.user.message_set.create(message=_("Your feedback has been saved successfully."))
-        return HttpResponseRedirect(request.POST.get('next', request.META.get('HTTP_REFERER', '/')))
+        messages.success(request, _("Your feedback has been saved successfully."))
+        return HttpResponseRedirect(form.cleaned_data.get('next', request.META.get('HTTP_REFERER', '/')))
     return render_to_response(template_name, {'feedback_form': form}, context_instance=RequestContext(request))
 
